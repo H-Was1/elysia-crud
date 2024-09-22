9f0ee74fa200f9cc9418f50ef74e3d5e61241a46
wasi <hammadwasi.b@gmail.com>
2024-09-22 22:31:38 +0500

    middlewares: ❌🔕❌🔕➡️➡️➡️

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,7 +1,7 @@
 import Elysia, { error, t } from "elysia";
 import { db } from "../drizzle/db";
 import { UserTable } from "../drizzle/schema";
-import { eq } from "drizzle-orm";
+import { and, eq, or } from "drizzle-orm";
 import bearer from "@elysiajs/bearer";
 import { postBody } from "../utils";
 const userRouter = new Elysia({ prefix: "/user" }).use(bearer());
@@ -49,8 +49,87 @@ userRouter.get(
   }
 );
 
+userRouter.post(
+  "/",
+  async ({ error, body, set, bearer }) => {
+    const { name, age, email } = body;
+    try {
+      console.log(bearer);
+
+      // Check for existing user
+      const existingUser = await db.query.UserTable.findFirst({
+        where: eq(UserTable.email, email),
+      });
+
+      if (existingUser) {
+        return error(400, "Email already exists");
+      }
+
+      // Insert new user
+      const [newUser] = await db
+        .insert(UserTable)
+        .values({
+          name,
+          email,
+        })
+        .returning();
+
+      return newUser;
+    } catch (err: any) {
+      // Handle unexpected errors
+      return error(500, err.message);
+    }
+  },
+
+  {
+    body: postBody,
+    detail: {
+      summary: "Create a new user",
+      tags: ["Auth"],
+      responses: {
+        201: {
+          description: "User created successfully",
+        },
+        400: {
+          description: "Invalid request",
+        },
+        500: {
+          description: "Internal server error",
+        },
+        422: {
+          description: "Validation failed",
+        },
+      },
+    },
+    type: "application/json",
+    security: [{ BearerAuth: [] }],
+  }
+);
+
+userRouter.put("/:id", () => "PUT", {
+  detail: {
+    summary: "Update a user",
+    tags: ["Auth"],
+  },
+});
+
 userRouter
-  .onBeforeHandle(({ set }) => {
+  .onRequest(async ({ set, error }) => {
+    // add data into body
+    set.headers["name"] = "User";
+  })
+  .get(
+    "/:id",
+    ({ headers }) => {
+      return { goal: "yoho" };
+    },
+    {
+      detail: {
+        summary: "Delete a user",
+        tags: ["Auth"],
+      },
+    }
+  ).onBeforeHandle(({ set }) => {
     console.log("1");
   })
   .get(
